189da6e36e9ff7791ec8aca3f148610024bd1951
Mon Pacleb <ramon.pacleb@1210servicesinc.ph>
2024-03-20 17:39:36 +0800

    sql changes

--- app/Models/Job_post_vw_model.php
+++ app/Models/Job_post_vw_model.php
@@ -516,7 +516,7 @@ class Job_post_vw_model extends Model {
         //--------------------------------add template--------------------------------
 
         if(!$data['record_header']['inactive'] && $data['table_name'] == 'ojob_post'){
-            $this->db->query("UPDATE ".$data['table_name']." SET date_posted = '".$data['record_header']['job_start_date']." '.DATE_FORMAT(NOW(),'%m/%d/%Y %h:%i %p')
+            $this->db->query("UPDATE ".$data['table_name']." SET date_posted = ".$data['record_header']['job_start_date'].".DATE_FORMAT(NOW(),'%m/%d/%Y %h:%i %p')
                   WHERE id = '".$id."'");
 
             //--------------------------------update template--------------------------------
